ba3d4d86c301da57f91a4f21ec9e399947e02dc4
mario mora <jorgemariomoramolina@gmail.com>
2024-04-13 20:43:12 -0500

    objects in javascript

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      name: 'diego',    
+      name: { firstName: 'diego', lastName: 'mora'},    
       company: 'j.town'
     }
 
@@ -20,10 +20,12 @@ class App extends Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi {this.state.name} , i work at {this.state.company}
+          hi {this.state.name.firstName} {this.state.name.lastName} , i work at {this.state.company}
         </p>
         <button onClick={ ()=> {
-          this.setState({name: 'mario'})}}> entrar</button>
+          this.setState({name: {firstName: 'mario', lastName: 'mora'}})
+          console.log(this.state)
+          }}> entrar</button>
     
       </header>
     </div>
