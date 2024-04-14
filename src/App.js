966ae1a880b696556baa7a7b0695a5b7aba495c6
mario mora <jorgemariomoramolina@gmail.com>
2024-04-13 20:18:43 -0500

    button changer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+
+import { Component } from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: 'diego',    
+      company: 'j.town'
+    }
+
+  }
+
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hi {this.state.name} , i work at {this.state.company}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={ ()=> {
+          this.setState({name: 'mario'})}}> entrar</button>
+    
       </header>
     </div>
   );
+  }  
 }
 
 export default App;
