c16eceed98366fceb25a4997835aa019b037409c
mario mora <jorgemariomoramolina@gmail.com>
2024-04-16 21:36:09 -0500

    creating to componentDimount method

--- src/App.js
+++ src/App.js
@@ -8,27 +8,35 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'mario'
-        },
-        {
-          name: 'ricardo'
-        },
-        {
-          name: 'jesus'
-        }
-      ]
+      monsters: []
     }
 
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response)=> response.json())
+      .then((users)=>
+        this.setState(
+          ()=>{
+            return {monsters: users};
+
+          },
+          ()=>{
+            console.log(this.state)
+          }
+        ) 
+      
+      )
+  };
 
   render(){
     return (
     <div className="App">
       {
         this.state.monsters.map((monster)=>{
-          return <h1>{monster.name}</h1>
+          return <h1 key={monster.id}>
+            {monster.name}</h1>
 
         })
       }
