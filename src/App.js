074226af02901ba922f24b66779275ab1c89b9cb
mario mora <jorgemariomoramolina@gmail.com>
2024-04-18 15:17:37 -0500

    finishing to search input

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
 
   }
@@ -31,6 +32,12 @@ class App extends Component {
   };
 
   render(){
+    console.log('render')
+
+    const filteredMonsters = this.state.monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    })
+
     return (
     <div className="App">
       <input
@@ -40,19 +47,16 @@ class App extends Component {
       onChange={
         (event)=>{
           console.log(event.target.value)
-          const searchString = event.target.value.toLocaleLowerCase()
-          const filteredMonsters = this.state.monsters.filter((monster)=>{
-            return monster.name.toLocaleLowerCase().includes(searchString)
-          })
-
+          const searchField = event.target.value.toLocaleLowerCase()
+          
         this.setState(()=>{
-          return {monsters: filteredMonsters}
+          return {searchField}
         })
          }
       }
       />
       {
-        this.state.monsters.map((monster)=>{
+        filteredMonsters.map((monster)=>{
           return <h1 key={monster.id}>
             {monster.name}</h1>
 
